cf5ce1ad7ccb160fe97287a148862af70dacc6b0
Adrian Bielawski <adrian@bielaw.ski>
2020-05-25 11:13:42 +0100

    Created GameMenu component and img folder with all images needed

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameMenu } from './components/game-menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <GameMenu/>
     </View>
   );
 }
---
+++ components/game-menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, View, Text, Image} from 'react-native';
+
+export const GameMenu = () => {
+    return (
+        <View>
+            <Image source={require('../assets/img/logo.jpg')} />
+        </View>
+    )
+    
+}
