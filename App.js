107c314eb0c1f30e2a9d4fa8ee6a3bdd0a59400a
Adrian Bielawski <adrian@bielaw.ski>
2020-05-25 11:35:21 +0100

    globalStyles created

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from './styles/global-styles'
 import { GameMenu } from './components/game-menu';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <GameMenu/>
     </View>
   );
